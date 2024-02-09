2f8ce3eb04c587075ddd9536fbdaef290dacba42
Maadajibao <>
2024-02-09 14:43:27 +0000

    Add division function

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,13 +40,17 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === 'subtract') {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === 'division') {
+        displayDivisionQuestion(num1, num2)
+    }
+    else {
         alert(`Unknown game type; ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting `;
     }
@@ -93,6 +97,9 @@ function calculateCorrectAnswer() {
     }
     else if (operator === '-') {
         return [operand1 - operand2, "subtract"];
+    }
+    else if (operator === '/') {
+        return [operand1 / operand2, "division"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -144,5 +151,13 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
+function displayDivisionQuestion(operand1, operand2) {
 
+
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
+
+}
 
